e4e3d348b449db9ed7a542ef16e9f2cd68369516
François-Xavier Thomas <fx.thomas@gmail.com>
2019-12-04 22:14:32 +0100

    Remove unneeded dependency liquibase -> legacyDaoHelper

--- airsonic-main/src/main/java/org/airsonic/player/spring/DatabaseConfiguration.java
+++ airsonic-main/src/main/java/org/airsonic/player/spring/DatabaseConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
@@ -86,7 +85,6 @@ public class DatabaseConfiguration {
     }
 
     @Bean
-    @DependsOn("legacyDaoHelper")
     public SpringLiquibase liquibase(DataSource dataSource,
                                      @Value("${DatabaseMysqlMaxlength:512}")
                                      String mysqlVarcharLimit,
