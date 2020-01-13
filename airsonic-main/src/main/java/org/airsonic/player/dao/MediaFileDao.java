fa72fe1c3df82e2addd7287a674c9ebe18952dd6
François-Xavier Thomas <fx.thomas@gmail.com>
2020-01-13 22:51:40 +0100

    Log advanced search

--- airsonic-main/src/main/java/org/airsonic/player/dao/MediaFileDao.java
+++ airsonic-main/src/main/java/org/airsonic/player/dao/MediaFileDao.java
@@ -494,6 +494,7 @@ public class MediaFileDao extends AbstractDao {
     }
 
     public List<MediaFile> searchAdvancedAlbums(final String username, String query, int count) {
+        LOG.info("Running advanced album search for user {}: {} (limit {})", username, query, count);
         AdvancedSearchQuerySqlVisitor.SqlWhereClause clause = AdvancedSearchQuerySqlAlbumVisitor.toSql(username, query);
         String sql = clause.getSelectClause("media_file", prefix(QUERY_COLUMNS, "media_file"));
         List<Object> args = new ArrayList<>();
@@ -501,6 +502,7 @@ public class MediaFileDao extends AbstractDao {
     }
 
     public List<MediaFile> searchAdvancedSongs(final String username, String query, int count) {
+        LOG.info("Running advanced song search for user {}: {} (limit {})", username, query, count);
         AdvancedSearchQuerySqlVisitor.SqlWhereClause clause = AdvancedSearchQuerySqlMusicVisitor.toSql(username, query);
         String sql = clause.getSelectClause("media_file", prefix(QUERY_COLUMNS, "media_file"));
         List<Object> args = new ArrayList<>();
@@ -508,6 +510,7 @@ public class MediaFileDao extends AbstractDao {
     }
 
     public List<MediaFile> searchAdvancedArtists(final String username, String query, int count) {
+        LOG.info("Running advanced artist search for user {}: {} (limit {})", username, query, count);
         AdvancedSearchQuerySqlVisitor.SqlWhereClause clause = AdvancedSearchQuerySqlArtistVisitor.toSql(username, query);
         String sql = clause.getSelectClause("media_file", prefix(QUERY_COLUMNS, "media_file"));
         List<Object> args = new ArrayList<>();
